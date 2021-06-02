2a3d4bcecbab2c80184c959db6a1b7c2916e8575
Kiminaze <kiminaze@yahoo.de>
2021-06-02 07:28:02 +0200

    Update example with proper scriptname

--- examples/clientExample.lua
+++ examples/clientExample.lua
@@ -5,7 +5,7 @@
 --   "CALLBACK_NAME" needs to be a unique string.
 --   "..." can be any amount of values sent from the server.
 --   return any amount of values
-exports["Callbacks"]:Register("CALLBACK_NAME", function(...)
+exports["kimi_callbacks"]:Register("CALLBACK_NAME", function(...)
 	-- do something here
 
 	return ANY, VALUES, SEPARATED_BY_COMMA
@@ -15,34 +15,34 @@ end)
 --   "CALLBACK_NAME" must be the same name as defined when registering the callback on server side.
 --   "..." can be any amount of values to send to the server.
 --   Returns any amount of values from the server.
-local data1, data2, data3 = exports["Callbacks"]:Trigger("CALLBACK_NAME", ...)
+local data1, data2, data3 = exports["kimi_callbacks"]:Trigger("CALLBACK_NAME", ...)
 
 -- Trigger a server callback with a custom timeout.
 --   "CALLBACK_NAME" must be the same name as defined when registering the callback on server side.
 --   "timeout" is the amount of time before the callback is cancelled (in ms).
 --   "..." can be any amount of values to send to the server.
 --   Returns any amount of values from the server.
-local data1, data2, data3 = exports["Callbacks"]:TriggerWithTimeout("CALLBACK_NAME", timeout ...)
+local data1, data2, data3 = exports["kimi_callbacks"]:TriggerWithTimeout("CALLBACK_NAME", timeout ...)
 
 
 
 -- EXAMPLES
 
 --   Return player position to server.
-exports["Callbacks"]:Register("getPlayerPosition", function()
+exports["kimi_callbacks"]:Register("getPlayerPosition", function()
 	local position = GetEntityCoords(PlayerPedId())
 
 	return position
 end)
 --   Return player position and heading to server.
-exports["Callbacks"]:Register("getPlayerPositionAndHeading", function()
+exports["kimi_callbacks"]:Register("getPlayerPositionAndHeading", function()
 	local playerPos = GetEntityCoords(PlayerPedId())
 	local heading = GetEntityHeading(PlayerPedId())
 
 	return playerPos, heading
 end)
 --   Return distance from a player to a given point to server.
-exports["Callbacks"]:Register("getPlayerDistanceToPosition", function(position)
+exports["kimi_callbacks"]:Register("getPlayerDistanceToPosition", function(position)
 	local playerPos = GetEntityCoords(PlayerPedId())
 
 	local distance = Vdist(playerPos.x, playerPos.y, playerPos.z, position.x, position.y, position.z)
@@ -52,14 +52,14 @@ end)
 
 --   Get player money from server.
 function GetMoney()
-    local cash, bank = exports["Callbacks"]:Trigger("getMoney")
+    local cash, bank = exports["kimi_callbacks"]:Trigger("getMoney")
 
     return cash, bank
 end
 
 --   Get player cash money from server with a timeout.
 function GetCashMoneyTimeout()
-    local cashMoney = exports["Callbacks"]:TriggerWithTimeout("getMoney", 500, "cash")
+    local cashMoney = exports["kimi_callbacks"]:TriggerWithTimeout("getMoney", 500, "cash")
 
 	if (cashMoney ~= nil) then
 		return cashMoney
