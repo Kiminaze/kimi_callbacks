d5dc41767f67ce87a83e8171e7e8ae67461fc95d
Kiminaze <kiminaze@yahoo.de>
2022-12-05 15:10:33 +0100

    Update version and reformat

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,16 @@
-fx_version 'cerulean'
-games { 'gta5' }
+fx_version "cerulean"
+games { "gta5" }
 
-author 'Philipp Decker'
-description 'Easy to use server and client callbacks!'
-version '1.1.2'
+author "Philipp Decker"
+description "Easy to use server and client callbacks!"
+version "1.2.0"
+
+lua54 "yes"
 
 server_scripts {
-	'server/ServerCallback.lua'
+	"server/ServerCallback.lua"
 }
 
 client_scripts {
-	'client/ClientCallback.lua'
+	"client/ClientCallback.lua"
 }
