11cd433ee8117daa484e8a7ce525b2a858c1225b
Kiminaze <kiminaze@yahoo.de>
2024-05-07 17:21:50 +0200

    Updated version to 1.4.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,15 @@
+
 fx_version "cerulean"
 games { "gta5" }
 
 author "Philipp Decker"
 description "Easy to use server and client callbacks!"
-version "1.3.0"
+version "1.4.0"
 
 lua54 "yes"
 
 server_scripts {
+	"server/versionChecker.lua",
 	"server/ServerCallback.lua"
 }
 
